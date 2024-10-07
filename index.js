4bbde7e9d7765fd908c04d70efba30389f818718
Kirthick27 <147175445+Kirthick27@users.noreply.github.com>
2024-10-07 14:02:02 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// CALCULATOR
+
+const display = document.getElementById("display");
+function appendToDisplay(input){
+display.value += input;
+}
+function clearDisplay(){
+display.value = "";
+}
+function calculate(){
+try{
+display.value = eval(display.value);
+}
+catch(error) {
+display.value = "Error";
+}
+}
